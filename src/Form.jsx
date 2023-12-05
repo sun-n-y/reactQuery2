3404e972be12df43bd162a8aaa3e3965f73b4aa4
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-05 16:11:28 -0500

    add use mutation

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,22 +1,29 @@
+import { useMutation } from '@tanstack/react-query';
 import { useState } from 'react';
+import { authFetch } from './utils';
 
 const Form = () => {
   const [newItemName, setNewItemName] = useState('');
 
+  const { mutate: createTask, isLoading } = useMutation({
+    mutationFn: (taskTitle) => authFetch.post('/', { title: taskTitle }),
+  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    createTask(newItemName);
   };
   return (
     <form onSubmit={handleSubmit}>
       <h4>task bud</h4>
-      <div className='form-control'>
+      <div className="form-control">
         <input
-          type='text '
-          className='form-input'
+          type="text "
+          className="form-input"
           value={newItemName}
           onChange={(event) => setNewItemName(event.target.value)}
         />
-        <button type='submit' className='btn'>
+        <button type="submit" className="btn" disabled={isLoading}>
           add task
         </button>
       </div>
